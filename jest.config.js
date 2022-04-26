7edc91379604210f28afabc393582b2d16e8b3fc
Zach DeGeorge <zach@as3ics.com>
2022-04-26 17:26:51 -0400

    added coverage report

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,13 @@ const tsPreset = require("ts-jest/jest-preset");
 const mongodbPreset = require("@shelf/jest-mongodb/jest-preset");
 
 module.exports = {
+  verbose: true,
   ...tsPreset,
   ...mongodbPreset,
-  verbose: true,
   globalSetup: "./node_modules/@shelf/jest-mongodb/setup.js",
   globalTeardown: "./node_modules/@shelf/jest-mongodb/teardown.js",
   transformIgnorePatterns: ["/node_modules/"],
+  collectCoverage: true,
+  coverageReporters: ["json", "html"],
+  coverageDirectory: "./coverage",
 };
